b76048494750489eeefdd6eb283697cc990c684f
Gabo@DESKTOP-P83O0FL.(none) <lissie.sifo@gmail.com>
2025-02-21 21:47:09 -0600

    :sparkles: Make the menu with the options

--- index.py
+++ index.py
@@ -8,3 +8,11 @@ eur = [1.05, 21.28]
 # mxn[1] para convertir de mxn a usd
 # eur[0] para convertir de eur a usd
 # eur[1] para convertir de eur a mxc
+
+#Mostrar al usuario el menu
+def showMenu(): 
+    print("Bienvenido a tu conversor de moneda")
+    print("1. Convertir moneda")
+    print("2. Ver tasas de cambio")
+    print("3. Actualizar tasas")
+    print("4. Salir")
