f58f5f1c37990589b9a5b18519be80acbe89e52b
Gabo@DESKTOP-P83O0FL.(none) <lissie.sifo@gmail.com>
2025-02-21 21:43:15 -0600

    Define the exchange currency

---
+++ index.py
@@ -0,0 +1,10 @@
+usd = [20.28, 0.95]
+mxn = [0.049, 0.047]  
+eur = [1.05, 21.28] 
+
+# usd[0] para convertir de usd a mxn
+# usd[1] para convertir de usd a eur
+# mxn[0] para convertir de mxn a eur
+# mxn[1] para convertir de mxn a usd
+# eur[0] para convertir de eur a usd
+# eur[1] para convertir de eur a mxc
